95b18d25e52d10e3ca6d3341ba7136440b7d1475
MY PC <MY PC@DESKTOP-DL2BNCG>
2024-03-23 14:32:17 -0500

    Agregar archivos de proyecto.

---
+++ FondodeSalud/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FondodeSalud
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FondodeSalud/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace FondodeSalud
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
